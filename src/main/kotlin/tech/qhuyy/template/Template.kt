1371604b025522b2b5a47a061f9cdd6e8e9ef3f5
quangg huyy <hqng05@gmail.com>
2026-06-23 12:18:57 +0700

    Create Template class for Bukkit plugin [skip ci]

---
+++ src/main/kotlin/tech/qhuyy/template/Template.kt
@@ -0,0 +1,14 @@
+package tech.qhuyy.template
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Template : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
