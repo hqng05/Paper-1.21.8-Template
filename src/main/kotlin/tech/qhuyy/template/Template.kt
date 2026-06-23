cd14785f2844e55432796a0f417ce4ba8b42c01c
quangg huyy <hqng05@gmail.com>
2026-06-23 12:41:38 +0700

    Initialize ConfigManager and MetricsManager on enable [skip ci]

--- src/main/kotlin/tech/qhuyy/template/Template.kt
+++ src/main/kotlin/tech/qhuyy/template/Template.kt
@@ -1,11 +1,20 @@
 package tech.qhuyy.template
 
 import org.bukkit.plugin.java.JavaPlugin
+import tech.qhuyy.template.manager.ConfigManager
+import tech.qhuyy.template.metrics.MetricsManager
+
+private const val PLUGIN_ID = 987654321 // CHANGE IT
 
 class Template : JavaPlugin() {
+    lateinit var configManager: ConfigManager
+        private set
+    lateinit var metricsManager: MetricsManager
+        private set
 
     override fun onEnable() {
-        // Plugin startup logic
+        configManager = ConfigManager(this)
+        metricsManager = MetricsManager(this, PLUGIN_ID).also { it.start() }
     }
 
     override fun onDisable() {
